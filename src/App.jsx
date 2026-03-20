9b9f233a77b5602b515a86b416b3947c1103f712
simonestrinati <simone.strinati@outlook.it>
2026-03-20 21:13:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+
+function App() {
+  // const [stato, setStato] = useState(null)
+
+  return (
+    <>
+    <div>
+      <h1>Benvenuto nella nostra WebAPI</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
