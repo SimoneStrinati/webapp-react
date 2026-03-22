e5f59a3a31c2281a520dc03ebd3d7c624dbf9185
simonestrinati <simone.strinati@outlook.it>
2026-03-22 21:13:33 +0100

    componente Card con props e CSS

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,8 +1,23 @@
+import MovieCard from "../components/MovieCard";
+
 function MoviesList() {
+
+
+    const movieplaceholder = {
+        id: 1,
+        title: "Titolo del film 1",
+        director: "Regista",
+        abstract: "Descrizione del film 1",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
+    }
 
     return <>
 
         <h1>Lista completa dei film</h1>
+
+        <div className="cards-container">
+            <MovieCard movie={movieplaceholder} />
+        </div>
         
     </>
 
