140a8aab584c7473f545989c6c03559430a0bd96
simonestrinati <simone.strinati@outlook.it>
2026-03-22 22:19:37 +0100

    creazione reviewCard e implementazione

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import style from "./MovieCard.module.css";
+
+function ReviewCard({review}) {
+
+    const { id, name, vote, text, image } = review;
+
+    return <div className={style.card}>
+            <img src={image} />
+            <h2>{name}</h2>
+            <h3>Valutazione: {vote}/10</h3>
+            <p>{text}</p>
+
+            <Link to={`/movies`}>Torna alla lista dei film</Link>
+        </div>
+
+        
+
+};
+
+export default ReviewCard;
--- src/pages/MoviePageDetails.jsx
+++ src/pages/MoviePageDetails.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import ReviewCard from "../components/ReviewCard";
 
+function MoviePageDetails(){
 
-function MoviePageDetails() {
+    const reviewPlaceholder = {
+        id: 1,
+        name: "Nome dell'utente",
+        vote: 8,
+        text: "Recensione del film",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
+    }
 
     return <>
 
         <h1>Pagina di dettaglio del film</h1>
-        <Link to="/movies">Torna alla lista dei film</Link>
+
+        <ReviewCard review={reviewPlaceholder} />
 
     </>
 
